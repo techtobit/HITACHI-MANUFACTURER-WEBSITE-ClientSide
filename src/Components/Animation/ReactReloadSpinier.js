b73313c49802f252acc8d5ea09d6e9449c076ca4
unknown <43129513+techtobit@users.noreply.github.com>
2022-05-22 22:47:40 +0600

    React Spinier add

---
+++ src/Components/Animation/ReactReloadSpinier.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SpinnerCircular } from 'spinners-react';
+
+const ReactReloadSpinier = () => {
+ return (
+  <div className='max-w-full max-h-full grid justify-center '>
+   <SpinnerCircular />
+  </div>
+ );
+};
+
+export default ReactReloadSpinier;

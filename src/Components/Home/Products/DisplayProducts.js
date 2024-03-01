a1d89637fc231becbbb0b9513cac0790132989dd
codify <nurthern2016@gmail.com>
2024-03-01 17:16:16 +0600

    updated home product card design

--- src/Components/Home/Products/DisplayProducts.js
+++ src/Components/Home/Products/DisplayProducts.js
@@ -28,8 +28,11 @@ const DisplayProducts = ({ product }) => {
   }
   return (
     <div >
-      <div class="card lg:w-80 md:w-fit bg-base-100 shadow-xl"
-        onMouseEnter={e => {
+      <div class="card rounded-none lg:w-80 h-96 md:w-fit bg-base-100 "
+
+        >
+        <div className='border-[0.5px] relative  border-neutral hover:border-primary border-opacity:90 rounded-none'
+          onMouseEnter={e => {
           setStyle({
             display: 'block',
             text: 'center',
@@ -42,21 +45,28 @@ const DisplayProducts = ({ product }) => {
             display: 'none',
             transition: '0.3s'
           })
-        }}>
-        <figure className=''><img className='product-img' src={img} alt="product" /></figure>
-        <div class="card-body">
-          <h3 class="card-title">{name}</h3>
-          <div className="card-data grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-2">
-            <div class="badge badge-primary text-neutral font-bold">Price : ${price}</div>
-            <div class="badge badge-accent text-neutral font-bold border-1 border-primary">Stock: ${quantity}</div>
-          </div>
-          <button onClick={() => handelPurchase(_id)} class="card-actions w-full lg:justify-center py-2 " style={style} >
-            <div class="btn btn-primary w-full text-neutral font-bold">
+        }}
+        >
+        <figure className='m-5 object-cover '>
+          <img className='product-img ' src={img} alt="product" />
+          <button onClick={() => handelPurchase(_id)} class="card-actions w-[100%] h-[100%]  backdrop-opacity-10 backdrop-invert bg-white/30  absolute lg:justify-center" style={style} >
+          {/* style={style} */}
+            <div class="btn  btn-primary w-[50%] text-neutral font-bold">
               <FontAwesomeIcon icon={faCartArrowDown} className='text pr-2'></FontAwesomeIcon>
               Place Oder
             </div>
           </button>
+        </figure>
         </div>
+        <div class="py-[0.5">
+          <h3 class="product-model">name</h3>
+          <h3 class="product-name font-bold overflow-hidden ">{name}</h3>
+          {/* <div className="card-data grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-2">
+            <div class="badge badge-primary text-neutral font-bold">Price : ${price}</div>
+            <div class="badge badge-accent text-neutral font-bold border-1 border-primary">Stock: ${quantity}</div>
+          </div> */}
+        </div>
+
       </div>
     </div>
   );
--- src/Components/Home/Products/Products.js
+++ src/Components/Home/Products/Products.js
@@ -8,7 +8,7 @@ const Products = () => {
  const [products] = LoadProducts();
 
  return (
-  <div className='bg-accent lg:p-26 md:p-20 p-10  '>
+  <div className='bg-secondary lg:p-26 md:p-20 p-10  '>
    <SubHeading>Latest Product</SubHeading>
    <SectionHeading> New Products On Market</SectionHeading>
    <div className="products grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-20 gap-10 sm:gap-10 justify-center justify-items-center items-center">
